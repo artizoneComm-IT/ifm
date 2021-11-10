e53a4e9c24fb56a3d22d0007f6b36a401a37af15
Charla19 <charlap20.aps2a@gmail.com>
2021-11-10 10:52:03 +0300

    views

---
+++ views/formations.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ArtiZone</title>
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/938e38e4e6.js" crossorigin="anonymous"></script>    
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.6.2/animate.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/index.css">    
+
+</head>
+<body>
+    
+</body>
+</html>
