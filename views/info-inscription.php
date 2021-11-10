9ff69263ffe4e6f2a903215473befcba698e6096
Charla19 <charlap20.aps2a@gmail.com>
2021-11-10 19:52:33 +0300

    inscription

--- views/info-inscription.php
+++ views/info-inscription.php
@@ -328,6 +328,7 @@
     </form>
     </section>
     <br>
+
     <br>
         <script type="text/javascript" src="../assets/js/jquery.js"></script>
         <script type="text/javascript" src="../assets/js/popper.js"></script>
