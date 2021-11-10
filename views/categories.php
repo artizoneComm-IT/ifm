e36455bba29761664413a754664495daeb48b220
naziradidier <nazirahp20.aps1b@gmail.com>
2021-11-10 10:49:11 +0300

    categorie ry reto ah

---
+++ views/categories.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="categori.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Categories</title>
+</head>
+<body>
+    <div class="container">
+        <h1 style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">Catégories des Arts </h1>
+        <p style="font-size: xx-large; font-family: Georgia, 'Times New Roman', Times, serif;">
+          Veuillez séléctionner votre catégorie s'il vous plait!
+        </p>
+    </div>
+    <div class="container" style="margin-top: 65px;">
+        <div class="row" >
+            <div class="card" style="width: 15rem; margin-right: 50px; margin-left: 150px; box-shadow:0 0 0.5em #888888;">
+                <a href="seConnecter.php">
+                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body">
+                  <h5 class="card-title" style="text-align:center;">Peinture/Hosodoko</h5>
+                    
+                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+          </div>
+            <div class="card" style="width: 15rem; margin-right: 50px; box-shadow:0 0 0.5em #888888;">
+                <a href="seConnecter.php">
+                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body">
+                  <h5 class="card-title" style="text-align:center;">Dessin/Sary</h5>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem; box-shadow:0 0 0.5em #888888;">
+                <a href="seConnecter.php">
+                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body">
+                  <h5 class="card-title" style="text-align:center;">Sculpture/Sary sikotra</h5>
+                </div>
+          </div>
+        </div>
+        <div class="row" style="margin-top: 25px;">
+              <div class="card" style="width: 15rem; margin-right: 50px; margin-left: 150px; box-shadow:0 0 0.5em #888888;">
+                  <a href="seConnecter.php">
+                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+                  </a>
+                  <div class="card-body">
+                    <h5 class="card-title" style="text-align:center;">Vannerie/Rary</h5>
+                  </div>
+            </div>
+              <div class="card" style="width: 15rem; margin-right: 50px; box-shadow:0 0 0.5em #888888;">
+                  <a href="seConnecter.php">
+                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+                  </a>
+                  <div class="card-body">
+                    <h5 class="card-title" style="text-align:center;">Coutûre/Zaitra</h5>
+                  </div>
+            </div>
+              <div class="card" style="width: 15rem; box-shadow:0 0 0.5em #888888;">
+                  <a href="seConnecter.php">
+                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+                  </a>
+                  <div class="card-body">
+                    <h5 class="card-title" style="text-align:center;">Tissage/Tenona</h5>
+                  </div>
+            </div>
+        </div>                                                                                                                                                                                                                                                                                                                                                                                                                                  
+      </div>
+    
+</body>
+</html>
