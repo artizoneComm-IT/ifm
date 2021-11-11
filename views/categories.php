807e9609abaf5990b5fe940d538e7b81b7eb7200
Charla19 <charlap20.aps2a@gmail.com>
2021-11-11 22:30:01 +0300

    update page categories

--- views/categories.php
+++ views/categories.php
@@ -4,71 +4,101 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="categori.css">
+    <link rel="stylesheet" href="../assets/css/categori.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Categories</title>
+    <!-- font link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <h1 style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">Catégories des Arts </h1>
-        <p style="font-size: xx-large; font-family: Georgia, 'Times New Roman', Times, serif;">
-          Veuillez séléctionner votre catégorie s'il vous plait!
-        </p>
-    </div>
-    <div class="container" style="margin-top: 65px;">
-        <div class="row" >
-            <div class="card" style="width: 15rem; margin-right: 50px; margin-left: 150px; box-shadow:0 0 0.5em #888888;">
-                <a href="seConnecter.php">
-                    <img src="nazirah.jpg" class="card-img-top" alt="...">
-                </a>
-                <div class="card-body">
-                  <h5 class="card-title" style="text-align:center;">Peinture/Hosodoko</h5>
-                    
-                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                </div>
+    <div class="container-fluid ">
+      <div class="row">
+        <nav class="col navbar navbar-expand-lg " style="background-color:rgba(23, 87, 124, 0.315)">
+          <a class="navbar-brand" href="...">
+            <img src="../assets/image/logo/logobg.png" width="120" height="70" alt="Artizone's logo">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div id="navbarContent" class="collapse navbar-collapse" >
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html" style="color:white; font-height:15px">Retour à l'accueil</a>
+              </li>
+            </ul>
           </div>
-            <div class="card" style="width: 15rem; margin-right: 50px; box-shadow:0 0 0.5em #888888;">
-                <a href="seConnecter.php">
-                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+        </nav>
+      </div>
+    </div>
+    <div class="container" style="margin-top:2%" >
+        <p style="font-size: xx-large; font-family: Georgia, 'Times New Roman', Times, serif;">
+          Veuiller faire le choix de votre catégorie
+        </p>
+        <div class="row " >
+          <div class="w-27 col-12 col-lg-4">
+              <div class="card" style="box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5), 0 2px 10px 0 rgba(0, 0, 0, 1)" >
+                  <a href="#">
+                      <img src="../assets/image/bg/nazirah.jpg" class="card-img-top" alt="...">
+                  </a>
+                  <div class="card-body">
+                    <h5 class="card-title" style="text-align:center;">Peinture/Hoso-doko</h5>
+                      
+                      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                  </div>
+              </div>
+          </div>
+          <div class="w-27 col-12 col-lg-4">
+            <div class="card" style="box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5), 0 2px 10px 0 rgba(0, 0, 0, 1)">
+                <a href="#">
+                    <img src="../assets/image/bg/nazirah.jpg" class="card-img-top" alt="...">
                 </a>
                 <div class="card-body">
                   <h5 class="card-title" style="text-align:center;">Dessin/Sary</h5>
                 </div>
             </div>
-            <div class="card" style="width: 15rem; box-shadow:0 0 0.5em #888888;">
-                <a href="seConnecter.php">
-                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+          </div>
+          <div class="w-27 col-12 col-lg-4">
+            <div class="card" style="box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5), 0 2px 10px 0 rgba(0, 0, 0, 1)" >
+                <a href="#">
+                    <img src="../assets/image/bg/nazirah.jpg" class="card-img-top" alt="...">
                 </a>
                 <div class="card-body">
                   <h5 class="card-title" style="text-align:center;">Sculpture/Sary sikotra</h5>
                 </div>
+            </div>
           </div>
-        </div>
-        <div class="row" style="margin-top: 25px;">
-              <div class="card" style="width: 15rem; margin-right: 50px; margin-left: 150px; box-shadow:0 0 0.5em #888888;">
-                  <a href="seConnecter.php">
-                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+          <div class="w-27 col-12 col-lg-4">
+              <div class="card" style="box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5), 0 2px 10px 0 rgba(0, 0, 0, 1)" >
+                  <a href="#">
+                    <img src="../assets/image/bg/nazirah.jpg" class="card-img-top" alt="...">
                   </a>
                   <div class="card-body">
                     <h5 class="card-title" style="text-align:center;">Vannerie/Rary</h5>
                   </div>
-            </div>
-              <div class="card" style="width: 15rem; margin-right: 50px; box-shadow:0 0 0.5em #888888;">
-                  <a href="seConnecter.php">
-                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+              </div>
+          </div>
+          <div class="w-27 col-12 col-lg-4">
+              <div class="card" style="box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5), 0 2px 10px 0 rgba(0, 0, 0, 1)" >
+                  <a href="#">
+                    <img src="../assets/image/bg/nazirah.jpg" class="card-img-top" alt="...">
                   </a>
                   <div class="card-body">
                     <h5 class="card-title" style="text-align:center;">Coutûre/Zaitra</h5>
                   </div>
-            </div>
-              <div class="card" style="width: 15rem; box-shadow:0 0 0.5em #888888;">
-                  <a href="seConnecter.php">
-                    <img src="nazirah.jpg" class="card-img-top" alt="...">
+              </div>
+          </div>
+          <div class="w-27 col-12 col-lg-4">
+              <div class="card" style="box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.5), 0 2px 10px 0 rgba(0, 0, 0, 1)" >
+                  <a href="#">
+                    <img src="../assets/image/bg/nazirah.jpg" class="card-img-top" alt="...">
                   </a>
                   <div class="card-body">
                     <h5 class="card-title" style="text-align:center;">Tissage/Tenona</h5>
                   </div>
-            </div>
+              </div>
+          </div>
         </div>                                                                                                                                                                                                                                                                                                                                                                                                                                  
       </div>
     
