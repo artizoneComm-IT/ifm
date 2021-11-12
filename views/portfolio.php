f74d1ece07a2c424449a544949d65f79e496256b
Charla19 <charlap20.aps2a@gmail.com>
2021-11-12 21:22:11 +0300

    Portfolio

--- views/portfolio.php
+++ views/portfolio.php
@@ -16,11 +16,23 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <title>Portfolio</title>
+
+    <style>
+      .banner-image{
+          background-image: url("../assets/image/art/profil.jpg");
+          background-size: cover;
+      }
+      .identite{
+          color: white;
+          margin-top: 50px
+      }
+      
+    </style>
   </head>
   <body>
     <div class="container-fluid">
-        <div class="row">
-            <nav class="col navbar navbar-expand-lg bg-dark navbar-dark" >
+        <div class="row" >
+            <nav class="col navbar  fixed-top navbar-expand-lg bg-dark navbar-dark" >
                 <a class="navbar-brand" href="...">
                     <img src="../assets/image/logo/logo1.png" width="120" height="70" alt="Site logo">
                 </a>
@@ -46,32 +58,39 @@
             </nav>
         </div>
     </div>
-    <div class="container" style="margin-top:100px">
+    <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+        <div class="content text-left">
+        <div class="container" >
         <div class="row">
-            <div class="col-12 col-lg-8">
-                <h1>RAKOTO Maria</h1>
-                <h2>Déssinatrice</h2>
-                <h2>contact</h2>
-                <h2>addresse</h2>
-                <h2>Déssimetier</h2>
-                <h2>description</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium iste ipsa, enim tenetur asperiores dolore quod dolorem unde molestias magnam.</p>
+            <div class="col-12 col-lg-5">
+                <div class="identite">
+                    <h1>RAKOTO Maria</h1>
+                    <h2>Déssinatrice</h2>
+                    <h2>contact</h2>
+                    <h2>addresse</h2>
+                    <h2>Déssimetier</h2>
+                    <h2>description</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium iste ipsa, enim tenetur asperiores dolore quod dolorem unde molestias magnam.</p>
+                </div>
             </div>
+            <div class="col-12 col-lg-3"></div>
             <div class="col-12 col-lg-4">
-                <img src="../assets/image/bg/profil.jpg" alt="profil" width="350" height="400" >
+                <img src="../assets/image/bg/profil.jpg" class="rounded-circle" alt="profil" width="350" height="400" >
             </div>
+        </div>
+    </div>
         </div>
     </div>
 
     <div class="container" style="margin-top:100px">
         <div class="row">
-            <div class="col-12 col-lg-6">
-                <img src="../assets/image/bg/profil.jpg" alt="profil" width="350" height="400">
+            <div class="col-12 col-lg-4">
+                <img src="../assets/image/bg/report.png"  alt="profil" width="350" height="350" >
             </div>
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-3"></div>
+            <div class="col-12 col-lg-5">
                 <h1>Formation</h1>
                 <h2>Déssinateur</h2>
-                <p>nmobre de formation</p>
                 <p>date</p>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa consequuntur nisi facere beatae eius, officia aliquid iusto illum voluptatem quasi.</p>
             </div>
@@ -80,21 +99,60 @@
     
     <div class="container" style="margin-top:100px">
         <div class="row">
-            <div class="col-12 col-lg-8">
+            <div class="col-12 col-lg-5">
                 <h1>Expérience</h1>
                 <h2>Déssinatrice</h2>
                 <p>date</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium iste ipsa, enim tenetur asperiores dolore quod dolorem unde molestias magnam.</p>
             </div>
+            <div class="col-12 col-lg-3"></div>
             <div class="col-12 col-lg-4">
-                <img src="../assets/image/bg/profil.jpg" alt="profil" width="350" height="400" >
+                <img src="../assets/image/bg/experience.png"  alt="profil" width="350" height="350" >
             </div>
         </div>
     </div>
     
     <div class="container" style="margin-top:100px">
-        <h1>conctact mandefa mail</h1>
-        <button disabled="mail"></button>
+        <div class="content text-left">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-2"></div>
+                    <div class="col-12 col-lg-6">
+                        <h1>Contacter</h1>
+                        <div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Adresse mail:</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                </div>
+                                <label for="exampleFormControlInput1" class="form-label">Mot de passe:</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Votre message:</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <div class="content text-center">
+                                <button type="button" class="btn btn-primary" style="margin-top:30px">Envoyer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
     </div>
+
+    
+
+    <script>
+        var nav = document.querySelector('nav');
+
+        window.addEventListener('scroll', function(){
+            if (window.pageYOffset > 100){
+                nav.classList.add('bg-dark', 'shadow');
+            }else {
+                nav.classList.remove('bg-dark', 'shadow')
+            }
+        })
+    </script> 
   </body>
 </html>
